bee46f3b18eaf98e20786faa3b4e0c9e9d61971e
Frank Gerold <FrankETSGerold@gmail.com>
2020-05-28 11:39:58 -0400

    better ps connection syntax

--- server/index.js
+++ server/index.js
@@ -22,10 +22,17 @@ const pgClient = new Pool({
   port: keys.pgPort
 });
 
-pgClient.on('error', () => console.log('Lost PG Connection'))
-
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch(err => console.log(err));
-
+// OLD:
+// pgClient.on('error', () => console.log('Lost PG Connection'))
+//
+// pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch(err => console.log(err));
+// 
+// FIX:
+pgClient.on('connect', () => {
+  pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err=>console.log(err))
+});
 
 // Redis client setup
 const redisClient = redis.createClient({
